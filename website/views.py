1d5bb0cce703b2d368fc6220c8f759ece7d6ebcd
SenPy <95858831+malika048@users.noreply.github.com>
2022-04-21 22:31:26 +0600

    21.04.2022

---
+++ website/views.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
+
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/')
+def main_page():
+    return render_template("main_page.html", user=current_user)
+
+
+@views.route('/home', methods=['GET', 'POST'])
+@login_required
+def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+        if len(note) < 1:
+            flash('Пожалуйста, введите что-нибудь.', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+    return render_template("home.html", user=current_user)
