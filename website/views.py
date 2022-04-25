bba6dda7353734249531b2c837275ff8f41ad877
SenPy <95858831+malika048@users.noreply.github.com>
2022-04-25 19:04:14 +0600

    25.04.2022
    Добавление функции удаления существующих заметок пользователем

--- website/views.py
+++ website/views.py
@@ -3,9 +3,26 @@ from flask_login import login_required, current_user
 from .models import Note
 from . import db
 import json
+import requests
+import geocoder
+from geopy.geocoders import Nominatim
 
 
 views = Blueprint('views', __name__)
+
+
+def get_weather(weather=[]):
+    coordinates = geocoder.ip('me').latlng
+    geolocator = Nominatim(user_agent="geoapiExercises")
+    location = geolocator.reverse(str(coordinates[0]) + "," + str(coordinates[1]))
+    address = location.raw['address']
+    city = address.get('city', '')
+    api = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=66e2fd0cd570079bfeabc5b1f959e0a9'
+    data = requests.get(api).json()
+    weather.append(city)
+    weather.append(data['weather'][0]['main'])
+    weather.append(int(data['main']['temp'] - 273.1))
+    return weather
 
 
 @views.route('/')
@@ -24,4 +41,17 @@ def home():
             new_note = Note(data=note, user_id=current_user.id)
             db.session.add(new_note)
             db.session.commit()
-    return render_template("home.html", user=current_user)
+    return render_template("home.html", user=current_user, weather=get_weather())
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
